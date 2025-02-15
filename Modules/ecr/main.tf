ec5ecb8e0566d94801a97ff66f8bafb5ea1bf4c4
praj-24-11 <137162184+praj-24-11@users.noreply.github.com>
2025-02-16 00:05:57 +0530

    Update main.tf

--- Modules/ecr/main.tf
+++ Modules/ecr/main.tf
@@ -10,11 +10,3 @@ resource "aws_ecr_repository" "this" {
     prevent_destroy = false
   }
 }
-
-output "repository_url" {
-  value = aws_ecr_repository.this.repository_url
-}
-
-output "repository_arn" {
-  value = aws_ecr_repository.this.arn
-}
