ee35db05d11a74c15b393ae092c1c0d4bd09b5c4
praj-24-11 <137162184+praj-24-11@users.noreply.github.com>
2025-02-16 00:04:27 +0530

    Update bucket

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -8,21 +8,24 @@ resource "random_id" "bucket_suffix" {
   byte_length = 8
 }
 
+# backend.tf
 terraform {
   backend "s3" {
-    bucket         = "unique8313878137891"
-    key            = "terraform/state/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-    acl            = "bucket-owner-full-control"
+    bucket         = "testbucket240612932137"  # Use your existing S3 bucket name
+    key            = "terraform/state/terraform.tfstate"  # State file path inside the bucket
+    region         = "us-east-1"  # Your AWS region
+    dynamodb_table = "terraform-locks"  # Optional, used to lock state (ensure it exists in the same region)
+    encrypt        = true  # Enable encryption for the state file
   }
 }
 
+# terraform-resources.tf
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+  name           = "terraform-locks"
+  hash_key       = "LockID"
+  read_capacity  = 5
+  write_capacity = 5
+
   attribute {
     name = "LockID"
     type = "S"
