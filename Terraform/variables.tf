e327223775e6b8808abaa71ea99bfface853c56b
praj-24-11 <137162184+praj-24-11@users.noreply.github.com>
2025-02-16 01:54:11 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -22,6 +22,7 @@ variable "private_subnet_cidrs" {
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"] # Add your AZs here
 }
 
 # ECS Variables
