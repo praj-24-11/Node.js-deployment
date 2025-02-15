4776ce704a2d9dc5b39ed1bfe9191da5b20a6b1a
praj-24-11 <137162184+praj-24-11@users.noreply.github.com>
2025-02-16 03:02:46 +0530

    Update outputs.tf

--- Modules/api_gateway/outputs.tf
+++ Modules/api_gateway/outputs.tf
@@ -7,6 +7,6 @@ output "api_gateway_invoke_url" {
 }
 
 output "api_gateway_arn" {
-  value = "arn:aws:apigateway:${var.region}::/restapis/${aws_api_gateway_rest_api.api.id}/stages/prod"
+  value = "arn:aws:apigateway:us-east-1::/restapis/${aws_api_gateway_rest_api.api.id}/stages/prod"
   description = "The ARN of the API Gateway stage"
 }
